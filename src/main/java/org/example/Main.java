74484985a7e5b2ab1d307c4d3a2c12bbce1530a6
yeonju <130127235+Dusenswf@users.noreply.github.com>
2026-09-14 17:33:40 +0900

    Implement sorting algorithm profiling in Main.java
    Added profiling functionality to evaluate sorting algorithms using BubbleSort and HeapSort.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // TODO : Profiler 실행해보기
+
+        Profiler profiler = new Profiler();
+
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(5, 2, 4, 1, 3, 9));
+        profiler.setData(input);
+        profiler.setLib(new BubbleSort());
+        profiler.runLib();
+        profiler.showResult();
+
+        // HeapSort도 같은 방식으로 이어서 평가 가능
+        profiler.setLib(new HeapSort());
+        profiler.runLib();
+        profiler.showResult();
     }
 }
